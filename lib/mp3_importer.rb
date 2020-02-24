dddbf6ce0de352578bebc27f8ab91c95037234cd
Lydell Gaines <dasnic70@gmail.com>
2020-02-24 14:15:56 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,15 +1,18 @@
+
 class MP3Importer
-  attr_reader :path
+
+  attr_accessor :path
 
   def initialize(path)
     @path = path
   end
 
   def files
-    @files ||= Dir.glob("#{path}/*.mp3").collect{ |f| f.gsub("#{path}/", "") }
+    @files ||= Dir.entries(@path).select {|song| !File.directory?(song) && song.end_with?(".mp3")}
   end
 
   def import
-    files.each{|f| Song.new_by_filename(f)}
+    files.each {|song| Song.new_by_filename(song)}
   end
+
 end
