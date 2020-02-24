fa9f7e9bbaa1c6bd8a6941ed6620851dc3d416c6
Lydell Gaines <dasnic70@gmail.com>
2020-02-24 14:08:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,26 +1,35 @@
 class Song
-
   attr_accessor :name, :artist
+
+  @@all = []
 
   def initialize(name)
     @name = name
   end
-  
-  def self.all      #class method all returns all instances of Artist class 
-    @@all
-  end 
-  
-  def self.new_by_filename(file_name)
-    song = file_name.split(" - ")[1]
-    artist = file_name.split(" - ")[0]
+
+  def self.new_by_filename(filename)
+    artist, song = filename.split(" - ")
     new_song = self.new(song)
     new_song.artist_name = artist
-    new_song
+    new_song.save
+  end
+
+  def self.find_by_artist(artist)
+    Song.all.select do | song |
+      song.artist == artist
+    end
+  end
+  
+  def self.all
+    @@all
   end
 
   def artist_name=(name)
     self.artist = Artist.find_or_create_by_name(name)
-    artist.add_song(self)
   end
-
+  
+  def save
+    @@all << self
+    self
+  end
 end
