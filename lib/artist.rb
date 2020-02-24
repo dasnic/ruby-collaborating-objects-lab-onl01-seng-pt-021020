fe1a8df077c917452c970c4f5b063749b0fa7649
Lydell Gaines <dasnic70@gmail.com>
2020-02-24 14:16:34 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,37 +1,40 @@
 class Artist
-  @@all = []
-  attr_accessor :name
+
+  attr_accessor :name, :songs
+  @@all =[]
 
   def initialize(name)
     @name = name
+    @songs = []
   end
 
-  def self.all
-    @@all    
-  end
-  
-  def songs
-    Song.find_by_artist(self)
-  end
-
-  def self.find_or_create_by_name(name)
-    self.find(name) ? self.find(name) : self.create(name)
-  end
-
-  def self.find(name)
-    self.all.find {|artist| artist.name == name }
-  end
-
-
-  def self.create(name)
-    self.new(name).tap {|artist| artist.save}
+  def add_song(song)
+    @songs << song
   end
 
   def save
-    @@all << self
+    @@all << self #saves the artist name to the all array
+  end
+
+  def self.all
+    @@all #displays the all array
+  end
+
+  def self.find_or_create_by_name(artist_name)
+    found_artist = self.all.find {|artist| artist.name == artist_name}
+    if found_artist
+      found_artist
+    else
+      new_artist = self.new(artist_name)
+      new_artist.save
+      new_artist
+    end
   end
 
   def print_songs
-    songs.each {|song| puts song.name}
+    @songs.each do |song|
+      puts song.name
+    end
   end
 end
+ 17  ruby-collaborating-objects-lab-v-000/lib/mp3_importer.rb 
